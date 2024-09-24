4b4e5b4e051e71e383750c790692653ba14b5a74
Gongjjin <gjyj5395@naver.com>
2024-09-24 16:02:02 +0900

    프로젝트 세팅

---
+++ src/test/java/com/example/filmpass/FilmpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
