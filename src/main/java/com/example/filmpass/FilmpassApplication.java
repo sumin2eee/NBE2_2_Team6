f7937c117b3ed56805a8309c5a33c887d98156fa
park sungjun <tjdwns3897@gmail.com>
2024-10-01 17:36:53 +0900

    예매, 좌석 엔티티 구현

--- src/main/java/com/example/filmpass/FilmpassApplication.java
+++ src/main/java/com/example/filmpass/FilmpassApplication.java
@@ -2,10 +2,8 @@ package com.example.filmpass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class FilmpassApplication {
 
     public static void main(String[] args) {
