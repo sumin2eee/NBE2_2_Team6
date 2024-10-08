2c2d3137baae802e98e8fcbdbc22f29b6d2f6a80
sumin2eee <sumin2eee@gmail.com>
2024-10-08 10:28:26 +0900

    a

--- src/main/java/com/example/filmpass/util/KeyGenerator.java
+++ src/main/java/com/example/filmpass/util/KeyGenerator.java
@@ -6,7 +6,7 @@ import java.util.Base64;
 public class KeyGenerator {
     public static void main(String[] args) {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] key = new byte[32]; // 256 bits
+        byte[] key = new byte[64]; // 256 bits
         secureRandom.nextBytes(key);
         String base64Key = Base64.getEncoder().encodeToString(key);
         System.out.println("Base64 Encoded Secret Key: " + base64Key);
